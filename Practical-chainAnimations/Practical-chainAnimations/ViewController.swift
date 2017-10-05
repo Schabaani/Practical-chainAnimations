d0adb9eaeaac26a24baefdce94344cfdb47832fa
Amish <amir.shabani68+gitlab@gmail.com>
2017-10-05 16:12:54 +0330

    initialize project

---
+++ Practical-chainAnimations/Practical-chainAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practical-chainAnimations
+//
+//  Created by Pishtaz on 10/5/17.
+//  Copyright © 2017 Sete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
