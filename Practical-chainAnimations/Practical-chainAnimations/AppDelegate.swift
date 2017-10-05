bdc2c65659a977c0a99cc21cf132e33ddea3b3fb
Amish <amir.shabani68+gitlab@gmail.com>
2017-10-05 17:33:24 +0330

    setup views programmatically

--- Practical-chainAnimations/Practical-chainAnimations/AppDelegate.swift
+++ Practical-chainAnimations/Practical-chainAnimations/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Practical-chainAnimations
 //
-//  Created by Pishtaz on 10/5/17.
+//  Created by Amir Shabani (Twitter: @_Schabaani) on 10/5/17.
 //  Copyright © 2017 Sete. All rights reserved.
 //
 
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+
+        let homeController = HomeController()
+        window?.rootViewController = UINavigationController(rootViewController: homeController)
         return true
     }
 
---
+++ Practical-chainAnimations/Practical-chainAnimations/HomeController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Practical-chainAnimations
+//
+//  Created by Amir Shabani (Twitter: @_Schabaani) on 10/5/17.
+//  Copyright © 2017 Sete. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNeedsStatusBarAppearanceUpdate()
+        setupViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIView.animate(withDuration: 1, animations: { 
+            self.backImage.alpha = 1
+        }) { (true) in
+            self.showMakeDateNow()
+        }
+    }
+    
+    //MARK:- back image
+    lazy var backImage: UIImageView = {
+        let image = UIImageView()
+        image.image = #imageLiteral(resourceName: "backHome")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.alpha = 0
+        return image
+    }()
+    //MARK:- make date text
+    lazy var makeDateText: UILabel = {
+        let label = UILabel()
+        label.text = "Make a date now"
+        label.textColor = .red
+        label.font = label.font.withSize(26)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.alpha = 0
+        return label
+    }()
+    //MARK:- login
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.alpha = 0
+        return button
+    }()
+    
+    //MARK:- Sing up
+    lazy var signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign Up", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.alpha = 0
+        return button
+    }()
+    
+    
+    private func setupViews() {
+        view.addSubview(backImage)
+        view.addSubview(makeDateText)
+        view.addSubview(loginButton)
+        view.addSubview(signUpButton)
+        
+        backImage.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        backImage.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        backImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backImage.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        makeDateText.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        makeDateText.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        makeDateText.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        makeDateText.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -50).isActive = true
+        loginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        loginButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        
+        signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 50).isActive = true
+        signUpButton.bottomAnchor.constraint(equalTo: loginButton.bottomAnchor).isActive = true
+        signUpButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+ 
+    }
+    
+    private func showMakeDateNow(){
+        UIView.animate(withDuration: 1, animations: {
+            self.makeDateText.alpha = 1
+        }) { (true) in
+            self.showButtons()
+        }
+    }
+    
+    private func showButtons(){
+        UIView.animate(withDuration: 1) { 
+            self.loginButton.alpha = 1
+            self.signUpButton.alpha = 1
+        }
+    }
+
+    
+    
+
+
+}
+
