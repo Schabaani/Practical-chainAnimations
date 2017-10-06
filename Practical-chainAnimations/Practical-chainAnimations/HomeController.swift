fa583b91477a6ef20b897081da6bcd31d5511b36
Amish <amir.shabani68+gitlab@gmail.com>
2017-10-06 10:15:53 +0330

    Hide navigationBar
    If you need to hide navigationBar, you can use this :         self.navigationController?.setNavigationBarHidden(true, animated: false)
    
    Pay attention: it hides navigationBar for all controllers

--- Practical-chainAnimations/Practical-chainAnimations/HomeController.swift
+++ Practical-chainAnimations/Practical-chainAnimations/HomeController.swift
@@ -14,6 +14,7 @@ class HomeController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
         setNeedsStatusBarAppearanceUpdate()
         setupViews()
     }
